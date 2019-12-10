585181545bc3738e4cb91b57115af910edf60524
iunnlu <ilhan.unnlu@gmail.com>
2019-12-10 15:04:48 +0100

    third commit

--- src/components/PostList.js
+++ src/components/PostList.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import UserHeader from './UserHeader';
 import { connect } from 'react-redux';
 import { fetchPosts } from '../actions';
 
---
+++ src/components/UserHeader.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import { fetchUser } from '../actions/index';
+import { connect } from 'react-redux';
+
+class UserHeader extends React.Component{
+    componentDidMount(){
+        this.props.fetchUser(this.props.userId);
+    }
+    render(){
+        const user = this.props.users.find(user => user.id === this.props.userId)
+        
+        if(!user)
+            return <div></div>
+
+        return(
+            <div className="header">
+                {user.name}
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return(
+        {users: state.users}
+    )
+}
+
+export default connect(
+    mapStateToProps,
+    { fetchUser }
+    )(UserHeader);
